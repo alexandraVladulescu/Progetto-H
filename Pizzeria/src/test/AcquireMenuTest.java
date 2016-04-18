e6e16175bbf41bbdb91ad78d8a7782695efa6184
frabru <Francesco@Francesco-PC>
2016-04-18 13:33:39 +0200

    Aggiunto test AqcuireMenu

--- Pizzeria/src/test/AcquireMenuTest.java
+++ Pizzeria/src/test/AcquireMenuTest.java
@@ -6,6 +6,8 @@
 package test;
 
 import data.MenuPizze;
+import i_o.AcquireTxtMenu;
+import i_o.AcquireXmlMenu;
 import java.io.IOException;
 
 /**
@@ -13,10 +15,19 @@ import java.io.IOException;
  * @author Francesco
  */
 public class AcquireMenuTest {
-    
+    // ./databases/pizze.txt
+    // ./databases/MenuPizze.xml
     public static void main(String[] args) throws IOException {
       MenuPizze menuPizze = new MenuPizze();
-      menuPizze.loadMenu();
+      menuPizze.loadMenu(new AcquireTxtMenu(),"./databases/pizze.txt");
         System.out.println(menuPizze.printAllPizzas());
+        
+          menuPizze.loadMenu(new AcquireXmlMenu(),"./databases/MenuPizze.xml");
+        System.out.println(menuPizze.printAllPizzas());
+        
+        
+        
+        
+        
     }
 }
