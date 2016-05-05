da791857fff28f1a8e4ddaba4bea67e78e1b5de5
francescobruno <fra90top@hotmail.com>
2016-05-05 15:14:14 +0200

    modifica Client

--- Pizzeria/src/data/ClientsManager.java
+++ Pizzeria/src/data/ClientsManager.java
@@ -10,16 +10,16 @@ import java.util.HashMap;
 public class ClientsManager {
     
     private ArrayList<Client> clients;
-    private HashMap<Client, Address> client_address_map;
+  
 
     public ClientsManager() {
         clients = new ArrayList<>();
-        client_address_map = new HashMap<>();
+      
     }
     
     public void addClient(Client client, Address address){
         clients.add(client);
-        client_address_map.put(client, address);
+       
     }
     
     
